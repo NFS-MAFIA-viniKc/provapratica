7b6711fbf1cbc3b3e6542f21bef35ee28cf92898
Vinicius <vinicuis@gmail.com>
2025-05-22 09:12:29 -0300

    codigos de erros

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const app = express();
+const server = express();
 const PORT = 8000;
 
-app.use(bodyParser.json());
-app.get('/', (req, res) => {
+server.use(bodyParser.json());
+server.get('/', (req, res) => {
     console.log('rota get');
     res.send('pintu');
 })
-app.listen(PORT, () => console.log(`Servidor esta rodando na porta ${PORT}`));
+
+server.get('/logs', (req, res) => {
+    const { id } = req.params;
+fstat.readFile('logs.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Erro', err);
+            return res.status(500).json('erro servidor');
+        }
+
+        const logs = data ? JSON.parse(data) : [];
+        const log = logs.find(log => log.id === id);
+
+        if (!log) {
+            return res.status(404).json('Log not found');
+        }
+
+        return res.json(log);
+    });
+})
+
+server.listen(PORT, () => console.log(`Servidor esta rodando na porta:${PORT}`));
 
