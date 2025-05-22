879605812b05001160acbe36b34b28eac9b65ddb
Vinicius <vinicuis@gmail.com>
2025-05-22 09:26:21 -0300

    registros

--- script.js
+++ script.js
@@ -10,9 +10,45 @@ server.get('/', (req, res) => {
     res.send('pintu');
 })
 
+server.post('/logs/registros', (req, res) => {
+    const body = req.body;
+
+    if (!body.name) {
+        return res.status(400).send('nome precisa ser preenchido');
+    }
+    const mensagem = {
+        id: randomUUID(),
+        mensagem: '',
+    }
+
+    fs.readFile('logs.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler arquivo:', err);
+            return res.status(500).send('erro servidor');
+        }
+
+        const logs = data ? JSON.parse(data) : [];
+        mensagem.mensagem = "criado com sucesso"
+        logs.push(mensagem);
+
+        fs.writeFile('logs.txt', JSON.stringify(logs, null, 2), (err) => {
+            if (err) {
+                console.error('erro ao escrever arquivo:', err);
+                return res.status(500).send('erro servidor');
+            }
+
+            return res.status(201).json(user);
+        });
+    });
+})
+
+
+    
+
 server.get('/logs', (req, res) => {
     const { id } = req.params;
-fstat.readFile('logs.txt', 'utf-8', (err, data) => {
+
+fs.readFile('logs.txt', 'utf-8', (err, data) => {
         if (err) {
             console.error('Erro', err);
             return res.status(500).json('erro servidor');
